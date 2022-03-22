b25b2ef30873d7110bb82b46769108cc9339bbef
Roman Zvonyk <jagerluck@gmail.com>
2022-03-22 10:48:55 +0200

    store setup (need to revisit)

--- src/handlers/marker.ts
+++ src/handlers/marker.ts
@@ -1,28 +1,7 @@
 import { Store } from '../store/store';
 
-/* 
-steps when open Pin:
-  - keep id of the pin, mb through data attr;
-  - check store for pin data by it's id, if none - call API;
-  - open popup slider with scrollable video or photo
-*/
-
 export const handleMarkerClick = (e: any) => {
-  new Store().getById(100);
-
-  // const div = document.createElement('div');
-  // Object.assign(div, {
-  //   className: 'popup',
-  //   height: 200,
-  //   width: 360,
-  //   style: `background: red; padding: 2rem;`,
-  //   innerHTML: 'some long text',
-  //   onclick: function () {
-  //     console.log('clicked', this);
-  //     // @ts-ignore
-  //     this.style.backgroundColor = this.style.backgroundColor === 'red' ? 'blue' : 'red';
-  //   },
-  // });
+  new Store().getById(100); // TODO: get id of a pin
 
   // L.popup({ autoClose: true, autoPan: true })
   //   .setLatLng(e.latlng)
